56982c5f20e14a8961c94dd4d8dbbf6e79541eed
sapphire <info@sapphire.sh>
2017-12-23 22:44:04 +0900

    add pixiv direct linker

---
+++ src/pixiv-direct-linker.js
@@ -0,0 +1,23 @@
+// ==UserScript==
+// @name         pixiv Direct Linker
+// @namespace    https://www.sapphire.sh/
+// @version      1.0
+// @description  Changes pixiv links to link destination directly
+// @author       sapphire
+// @match        https://www.pixiv.net/*
+// @updateURL    https://raw.githubusercontent.com/sapphiredev/UserScripts/master/src/pixiv-direct-linker.js
+// @grant        none
+// ==/UserScript==
+
+(function() {
+	'use strict';
+
+	Array.from(document.querySelectorAll('a')).forEach((e) => {
+		const url = e.href;
+
+		const match = url.match(/jump.php\?(.+)$/i);
+		if(match !== null) {
+			e.href = decodeURIComponent(match[1]);
+		}
+	});
+})();
