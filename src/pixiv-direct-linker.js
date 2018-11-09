bbd41ef76b2a790e4fac959e2eaba8c72fc73d85
sapphire <info@sapphire.sh>
2018-11-10 02:34:26 +0900

    update pixiv direct linker

--- src/pixiv-direct-linker.js
+++ src/pixiv-direct-linker.js
@@ -1,30 +1,30 @@
 // ==UserScript==
-// @name         pixiv Direct Linker
+// @name         pixiv direct linker
 // @namespace    https://www.sapphire.sh/
-// @version      1.1
-// @description  Changes pixiv links to link destination directly
+// @version      1.2
+// @description  convert pixiv links to direct links
 // @author       sapphire
 // @match        https://www.pixiv.net/*
 // @downloadURL  https://raw.githubusercontent.com/sapphiredev/UserScripts/master/src/pixiv-direct-linker.js
 // @grant        none
 // ==/UserScript==
 
-(function() {
+(() => {
 	'use strict';
 
-    const observer = new MutationObserver(() => {
-        Array.from(document.querySelectorAll('a')).forEach((e) => {
-            const url = e.href;
+	const observer = new MutationObserver(() => {
+		Array.from(document.querySelectorAll('a')).forEach((e) => {
+			const url = e.href;
 
-            const match = url.match(/jump.php\?(.+)$/i);
-            if(match !== null) {
-                e.href = decodeURIComponent(match[1]);
-            }
-        });
-    });
+			const match = url.match(/jump.php\?(url=)?(.+)$/i);
+			if(match !== null) {
+				e.href = decodeURIComponent(match[2]);
+			}
+		});
+	});
 
-    observer.observe(document.body, {
-        'childList': true,
-        'subtree': true,
-    });
+	observer.observe(document.body, {
+		'childList': true,
+		'subtree': true,
+	});
 })();
