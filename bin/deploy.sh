b6fb425838316dc3cc6e34352b7462454785561e
sapphire <info@sapphire.sh>
2018-11-10 21:04:27 +0900

    update deploy.sh

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -2,10 +2,11 @@
 
 set -ex
 
+rm ./dist/.gitignore
 git config user.email "deploy@travis-ci.org"
 git config user.name "Deployment Bot (from Travis CI)"
-git remote add origin https://${GITHUB_TOKEN}@github.com/sapphiredev/UserScripts.git
+git remote set-url origin https://${GITHUB_TOKEN}@github.com/sapphiredev/UserScripts.git
 git checkout -b release
 git add ./dist
 git commit -m "travis-ci build: $TRAVIS_BUILD_NUMBER"
-git push origin release
+git push origin release -f
