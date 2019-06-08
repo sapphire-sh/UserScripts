e5a8851089ffeca665d0729b3ebb80a24a8552bf
sapphire <info@sapphire.sh>
2019-06-08 14:26:16 +0900

    delete suggested tweet remover

--- src/scripts/suggested-tweet-remover.ts
+++
@@ -1,4 +0,0 @@
-(() => {
-	GM_addStyle('#stream-items-id > li[data-suggestion-json^=\'{"suggestion_details":{"suggestion_type":\'] { display: none; }');
-	GM_addStyle('#stream-items-id > li[data-item-type="recap_entry"] { display: none; }');
-})();
