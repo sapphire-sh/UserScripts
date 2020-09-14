4a501c8552132f046e645e648c960d6ca0905a64
sapphire <info@sapphire.sh>
2020-09-14 12:39:40 +0900

    fix twitter image order

--- src/scripts/twitter-original-image-downloader.ts
+++ src/scripts/twitter-original-image-downloader.ts
@@ -40,15 +40,19 @@ import {
 	async function getImages(article: HTMLElement) {
 		await waitForElement('img[alt="Image"]');
 		const e = article.querySelectorAll<HTMLImageElement>('img[alt="Image"]');
-		return Array.from(e);
+		const images = Array.from(e);
+		if (images.length === 4) {
+			const t = images[1];
+			images[1] = images[2];
+			images[2] = t;
+		}
+		return images;
 	}
 
 	const articles = await getArticles();
 
 	for (const article of articles) {
-		console.log('article', article);
 		const images = await getImages(article);
-		console.log('images', images);
 		if (images.length === 0) { continue; }
 
 		const button = generateButton();
