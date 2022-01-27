b348bd8ff5b5b9db0ace7ef3bfb097bb4857b981
sapphire <info@sapphire.sh>
2022-01-28 01:34:38 +0900

    add twitter media warning remover

---
+++ src/scripts/twitter-media-warning-remover.ts
@@ -0,0 +1,13 @@
+(() => {
+    const observer = new MutationObserver(() => {
+        const elements = Array.from<HTMLElement>(document.querySelectorAll('article div[role="button"].r-173mn98'));
+        for (const element of elements) {
+            element.click();
+        }
+    });
+
+    observer.observe(document.body, {
+        'childList': true,
+        'subtree': true,
+    });
+})();
