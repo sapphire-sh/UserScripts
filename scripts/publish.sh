ad1d90b0c977a072f3ba599b13cab37f3664fb24
sapphire <info@sapphire.sh>
2022-08-18 03:17:26 +0900

    fix commit author

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -2,10 +2,10 @@
 
 set -ex
 
-git switch -c release
+git switch --orphan release
 git add ./dist
-git config user.name "github-actions"
-git config user.email "github-actions@github.com"
+git config user.name "github-actions[bot]"
+git config user.email "github-actions[bot]@users.noreply.github.com"
 git commit -m "publish: $GITHUB_RUN_NUMBER"
 git tag "$GITHUB_RUN_NUMBER"
 git push origin release -f
