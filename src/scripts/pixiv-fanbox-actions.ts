20cbe56a95ed19fc938a5da068863614bc9504f6
sapphire <info@sapphire.sh>
2022-12-19 02:12:54 +0900

    sanitize url

--- src/scripts/pixiv-fanbox-actions.ts
+++ src/scripts/pixiv-fanbox-actions.ts
@@ -1,47 +1,88 @@
 import { waitForElement } from '../helpers';
 
+enum PageType {
+	A,
+	B,
+}
+
 interface Links {
 	prevLink: string | null;
 	nextLink: string | null;
 }
 
-const generateButtons = (id: string, title: string, links: Links, images: HTMLAnchorElement[]): HTMLDivElement => {
+interface ParamsA {
+	type: PageType.A;
+	id: string;
+	username: string;
+}
+
+interface ParamsB {
+	type: PageType.B;
+	id: string;
+	title: string;
+	links: Links;
+	images: HTMLAnchorElement[];
+}
+
+const generateButtons = (params: ParamsA | ParamsB): HTMLDivElement => {
 	const div = document.createElement('div');
 	div.setAttribute('style', 'position:fixed;left:100px;top:100px;');
 
-	{
-		const button = document.createElement('button');
-		button.textContent = 'copy';
-		button.onclick = () => {
-			window.navigator.clipboard.writeText(`${id}_${title}`);
-		};
-		div.appendChild(button);
-	}
+	switch (params.type) {
+		case PageType.A: {
+			console.log('params', params);
 
-	{
-		const button = document.createElement('button');
-		button.textContent = `open (${images.length})`;
-		button.onclick = generateHandler(images);
-		div.appendChild(button);
-	}
+			const { id, username } = params;
+			{
+				const button = document.createElement('button');
+				button.textContent = 'sanitize';
+				button.onclick = () => {
+					window.location.href = `https://${username}.fanbox.cc/posts/${id}`;
+				};
+				div.appendChild(button);
+			}
 
-	if (links.prevLink !== null) {
-		const button = document.createElement('button');
-		button.textContent = 'prev';
-		button.onclick = () => {
-			div.remove();
-			location.href = links.prevLink!;
-		};
-		div.appendChild(button);
-	}
-	if (links.nextLink !== null) {
-		const button = document.createElement('button');
-		button.textContent = 'next';
-		button.onclick = () => {
-			div.remove();
-			location.href = links.nextLink!;
-		};
-		div.appendChild(button);
+			break;
+		}
+		case PageType.B: {
+			const { id, title, links, images } = params;
+
+			{
+				const button = document.createElement('button');
+				button.textContent = 'copy';
+				button.onclick = () => {
+					window.navigator.clipboard.writeText(`${id}_${title}`);
+				};
+				div.appendChild(button);
+			}
+
+			{
+				const button = document.createElement('button');
+				button.textContent = `open (${images.length})`;
+				button.onclick = generateHandler(images);
+				div.appendChild(button);
+			}
+
+			if (links.prevLink !== null) {
+				const button = document.createElement('button');
+				button.textContent = 'prev';
+				button.onclick = () => {
+					div.remove();
+					location.href = links.prevLink!;
+				};
+				div.appendChild(button);
+			}
+			if (links.nextLink !== null) {
+				const button = document.createElement('button');
+				button.textContent = 'next';
+				button.onclick = () => {
+					div.remove();
+					location.href = links.nextLink!;
+				};
+				div.appendChild(button);
+			}
+			break;
+		}
 	}
 
 	return div;
@@ -70,11 +111,18 @@ const getImages = (article: HTMLElement): HTMLAnchorElement[] => {
 	return Array.from(e);
 };
 
-const attach = async (id: string, title: string, links: Links) => {
+const attachA = async (id: string, username: string) => {
+	const article = await getArticle();
+
+	const buttons = generateButtons({ type: PageType.A, id, username });
+	article.appendChild(buttons);
+};
+
+const attachB = async (id: string, title: string, links: Links) => {
 	const article = await getArticle();
 	const images = getImages(article);
 
-	const buttons = generateButtons(id, title, links, images);
+	const buttons = generateButtons({ type: PageType.B, id, title, links, images });
 	article.appendChild(buttons);
 };
 
@@ -93,6 +141,34 @@ const getLinks = (response: any): Links => {
 };
 
 const main = () => {
+	const getArticleId = () => {
+		const match = window.location.pathname.match(/\/posts\/(\d+)/);
+		if (!match) {
+			return null;
+		}
+		return match[1];
+	};
+	const getUsername = () => {
+		const match = window.location.pathname.match(/\/@(.+?)\//);
+		if (!match) {
+			return null;
+		}
+		return match[1];
+	};
+
+	const articleId = getArticleId();
+	if (!articleId) {
+		return;
+	}
+
+	if (window.location.href.match(/^https:\/\/www\.fanbox\.cc\/\@/)) {
+		const username = getUsername();
+		if (!username) {
+			return;
+		}
+		return attachA(articleId, username);
+	}
+
 	const XHR = window.XMLHttpRequest;
 	// @ts-ignore
 	window.XMLHttpRequest = function () {
@@ -107,14 +183,6 @@ const main = () => {
 			if (xhr.responseURL.match(/post\.info/) === null) {
 				return;
 			}
-			const getArticleId = () => {
-				const match = window.location.pathname.match(/^\/posts\/(\d+)/);
-				if (!match) {
-					return null;
-				}
-				return match[1];
-			};
-			const articleId = getArticleId();
 			const response = JSON.parse(xhr.response).body;
 			if (articleId !== response.id) {
 				return;
@@ -122,7 +190,7 @@ const main = () => {
 			const id = response.id;
 			const title = response.title;
 			const links = getLinks(response);
-			attach(id, title, links);
+			attachB(id, title, links);
 		};
 		xhr.addEventListener('readystatechange', handleReadyStateChange, false);
 		return xhr;
