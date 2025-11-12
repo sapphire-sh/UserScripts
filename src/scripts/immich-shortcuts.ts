bec88b6e8745b86eac357bbb58e060ea809f4a58
sapphire <info@sapphire.sh>
2025-11-12 22:49:06 +0900

    update immich shortcuts

--- src/scripts/immich-shortcuts.ts
+++ src/scripts/immich-shortcuts.ts
@@ -1,5 +1,38 @@
+const updateRating = (rating: number) => {
+	const urls = ['/photos/', '/search/photos/'];
+
+	for (const url of urls) {
+		if (!window.location.pathname.startsWith(url)) {
+			continue;
+		}
+
+		const assetId = window.location.pathname.replace(url, '');
+
+		fetch(`/api/assets/${assetId}`, {
+			method: 'PUT',
+			headers: {
+				accept: 'application/json',
+				'content-type': 'application/json',
+			},
+			body: JSON.stringify({ rating }),
+		});
+	}
+};
+
 const handleKeyUp = (event: KeyboardEvent) => {
 	console.log('event', event);
+
+	switch (event.code) {
+		case 'Digit1':
+		case 'Digit2':
+		case 'Digit3':
+		case 'Digit4':
+		case 'Digit5': {
+			const rating = parseInt(event.code.replace('Digit', ''), 10);
+			updateRating(rating);
+			break;
+		}
+	}
 };
 
 const main = () => {
