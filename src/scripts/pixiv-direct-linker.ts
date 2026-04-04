41c6868d99ce34d4bd3ffb747c6ced879485f17f
sapphire <info@sapphire.sh>
2026-04-04 19:41:44 +0900

    refactor(pixiv-direct-linker): process only addedNodes in MutationObserver instead of querying all anchors

--- src/scripts/pixiv-direct-linker.ts
+++ src/scripts/pixiv-direct-linker.ts
@@ -1,11 +1,24 @@
+const replaceLink = (anchor: HTMLAnchorElement) => {
+	const match = anchor.href.match(/jump.php\?(url=)?(.+)$/i);
+	if (match?.[2] !== undefined && match[2] !== '') {
+		anchor.href = decodeURIComponent(match[2]);
+	}
+};
+
 const main = () => {
-	const observer = new MutationObserver(() => {
-		const anchors = Array.from(document.querySelectorAll('a'));
-		for (const anchor of anchors) {
-			const { href } = anchor;
-			const match = href.match(/jump.php\?(url=)?(.+)$/i);
-			if (match?.[2] !== undefined && match[2] !== '') {
-				anchor.href = decodeURIComponent(match[2]);
+	const observer = new MutationObserver((mutations) => {
+		for (const mutation of Array.from(mutations)) {
+			for (const node of Array.from(mutation.addedNodes)) {
+				if (!(node instanceof HTMLElement)) {
+					continue;
+				}
+				if (node instanceof HTMLAnchorElement) {
+					replaceLink(node);
+				} else {
+					for (const anchor of Array.from(node.querySelectorAll('a'))) {
+						replaceLink(anchor);
+					}
+				}
 			}
 		}
 	});
