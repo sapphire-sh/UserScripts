4b34051943810e14faa2fbecb8560117b35d86a3
sapphire <info@sapphire.sh>
2026-08-31 00:15:35 +0900

    refactor(misskey): split overscroll-behavior CSS fix into its own script (closes #52)

---
+++ src/scripts/misskey-overscroll-fix.ts
@@ -0,0 +1,5 @@
+GM_addStyle(`
+html, body, #misskey_app, ._pageContainer, ._pageScrollable, ._pageScrollableReversed {
+	overscroll-behavior-x: auto !important;
+}
+`);
--- src/scripts/misskey-restore-native-navigation.ts
+++ src/scripts/misskey-restore-native-navigation.ts
@@ -44,9 +44,3 @@ void (() => {
 })();
 
 export {};
-
-GM_addStyle(`
-html, body, #misskey_app, ._pageContainer, ._pageScrollable, ._pageScrollableReversed {
-	overscroll-behavior-x: auto !important;
-}
-`);
